7ec6ee7ee187e86b45f547d2ac58f54c1ba40b59
Aitor P. Iturri <aitor.iturri@gmail.com>
2014-06-14 00:07:10 +0200

    1st refactor of Mail class

--- unfilteredMMail/src/main/scala/com/logikujo/www/mail/package.scala
+++ unfilteredMMail/src/main/scala/com/logikujo/www/mail/package.scala
@@ -16,37 +16,102 @@ import Scalaz._
  *
  * com.logikujo.www.mail 6/04/14 :: 18:36 :: eof
  *
+ * TODO:
+ *  * Refactor this class, allow a Tagged MailSystem
  */
 package object mail {
-  type UnfilteredMailM = UnfilteredM[SendMail]
-
-  //def unfilteredMailM = liftM[SendMail]_
-  def unfilteredMailM = liftM[SendMail]((c: Configuration) => (new SendMail {
-    val config: Configuration = c
-  }).right[String])
+  trait MailSystem
 
   implicit def stringToSeq(single: String): Seq[String] = Seq(single)
 
   implicit def liftToOption[T](t: T): Option[T] = Some(t)
 
-  sealed abstract class MailType
+  sealed abstract class MailType {
+    def apply(): Email
+  }
 
-  case object Plain extends MailType
+  case object Plain extends MailType {
+    def apply() = new SimpleEmail()
+  }
 
-  case object Rich extends MailType
+  case object Rich extends MailType {
+    def apply() = new HtmlEmail()
+  }
 
-  case object MultiPart extends MailType
+  case object MultiPart extends MailType {
+    def apply() = new MultiPartEmail()
+  }
 
-  sealed case class Mail(
-                          from: (String, String), // (email -> name)
-                          to: Seq[String],
-                          cc: Seq[String] = Seq.empty,
-                          bcc: Seq[String] = Seq.empty,
-                          subject: String,
-                          message: String,
-                          richMessage: Option[String] = None,
-                          attachment: Option[(java.io.File)] = None
-                          )
+  case class Mail(
+    from: Option[(String, String)] = None,
+    to: Option[Seq[String]] = Seq.empty.some,
+    cc: Option[Seq[String]] = Seq.empty.some,
+    bcc: Option[Seq[String]] = Seq.empty.some,
+    subject: Option[String] = None,
+    message: Option[String] = None,
+    richMessage: Option[String] = None,
+    attachment: Option[(java.io.File)] = None) {
+    def from(v: (String,String)) = this.copy(from=v.some)
+    def to(v: Seq[String]) = this.copy(to=v.some)
+    def cc(v: Seq[String]) = this.copy(cc=v.some)
+    def bcc(v: Seq[String]) = this.copy(bcc=v.some)
+    def subject(v: String) = this.copy(subject=v.some)
+    def message(v: String) = this.copy(message=v.some)
+    def richMessage(v: String) = this.copy(richMessage=v.some)
+    def attachment(v:java.io.File) = this.copy(attachment=v.some)
+    val format: MailType =
+      if (attachment.isDefined) MultiPart
+      else if (richMessage.isDefined) Rich
+      else Plain
+  }
+
+  sealed trait MailSender {
+    val config: Configuration
+    val sendEmail = for {
+      localhost <- config.∨[String]("mail.localhost")
+      smtp <- config.∨[String]("mail.smtpServer")
+      port <- config.∨[Int]("mail.smtpPort")
+      user <- config.∨[String]("mail.smtpUser")
+      passwd <- config.∨[String]("mail.smtpPasswd")
+    } yield (mail: Mail) => {
+        for {
+          to <- mail.to
+          (fromAddress, fromName) <- mail.from
+          cc <- mail.cc
+          bcc <- mail.bcc
+          subject <- mail.subject
+          message <- mail.message
+          commonsMail: Email <- mail.format().some
+        } yield {
+          to foreach (commonsMail.addTo(_))
+          cc foreach (commonsMail.addCc(_))
+          bcc foreach (commonsMail.addBcc(_))
+          commonsMail.setFrom(fromAddress, fromName)
+          commonsMail.setSubject(subject)
+          commonsMail.setDebug(config.opt[Boolean]("mail.debug").getOrElse(true))
+          commonsMail.setStartTLSRequired(config.opt[Boolean]("mail.startTLSRequired").getOrElse(true))
+          commonsMail.setStartTLSEnabled(config.opt[Boolean]("startTLSEnabled").getOrElse(true))
+          commonsMail.setSocketConnectionTimeout(config.opt[Int]("mail.socketConnectionTimeout").getOrElse(4000))
+          commonsMail.setSocketTimeout(config.opt[Int]("mail.socketTimeout").getOrElse(4000))
+          commonsMail.setHostName(smtp)
+          commonsMail.setSmtpPort(port)
+          commonsMail.setAuthenticator(new DefaultAuthenticator(user, passwd))
+          commonsMail
+        }
+      }
+
+    def send(mail: Mail) = sendEmail(mail).map(m => mail.format match {
+      case Plain => m.setMsg(mail.message)
+      case Rich => new HtmlEmail().setHtmlMsg(mail.richMessage.get).setTextMsg(mail.message)
+      case MultiPart => {
+        val attachment = new EmailAttachment()
+        attachment.setPath(mail.attachment.get.getAbsolutePath)
+        attachment.setDisposition(EmailAttachment.ATTACHMENT)
+        attachment.setName(mail.attachment.get.getName)
+        new MultiPartEmail().attach(attachment).setMsg(mail.message)
+      }
+    })
+    }
 
   // Instantiate as much as possible when creating it
   sealed trait SendMail {
@@ -58,12 +123,7 @@ package object mail {
                              passwd: String,
                              localhost: String,
                              mail: Mail): Email = {
-      System.setProperty("com.logikujo.www.mail.smtp.localhost", localhost)
-      val format =
-        if (mail.attachment.isDefined) MultiPart
-        else if (mail.richMessage.isDefined) Rich
-        else Plain
-
+      //System.setProperty("com.logikujo.www.mail.smtp.localhost", localhost)
       val commonsMail: Email = format match {
         case Plain => new SimpleEmail().setMsg(mail.message)
         case Rich => new HtmlEmail().setHtmlMsg(mail.richMessage.get).setTextMsg(mail.message)
@@ -76,45 +136,6 @@ package object mail {
         }
       }
 
-      // TODO Set authentication from your configuration, sys properties or w/e
-
-      // Can't add these via fluent API because it produces exceptions
-      mail.to foreach (commonsMail.addTo(_))
-      mail.cc foreach (commonsMail.addCc(_))
-      mail.bcc foreach (commonsMail.addBcc(_))
-
-      val email = commonsMail.
-        setFrom(mail.from._1, mail.from._2).
-        setSubject(mail.subject)
-      email.setHostName(localhost)
-      email.setSmtpPort(port)
-      email.setAuthenticator(new DefaultAuthenticator(user, passwd));
-      email.setDebug(true)
-      email.setStartTLSRequired(true)
-      email.setStartTLSEnabled(true)
-      email.setSocketConnectionTimeout(4000)
-      email.setSocketTimeout(4000)
-      email
-    }
-
-    def apply(mail: Mail): Try[String] = {
-      println("------> " + config.opt[String]("mail.localhost"))
-      val commonEmail = for {
-        localhost <- config.opt[String]("mail.localhost")
-        smtp <- config.opt[String]("mail.smtpServer")
-        port <- config.opt[Int]("mail.smtpPort")
-        user <- config.opt[String]("mail.smtpUser")
-        passwd <- config.opt[String]("mail.smtpPasswd")
-      //debugLevel <- config.opt[String]("com.logikujo.www.mail.debugLevel")
-      } yield {
-        val commonEmail = prepareEmail(smtp, port, user, passwd, localhost, mail)
-        commonEmail.setDebug(config.opt[Boolean]("mail.debug").getOrElse(true))
-        commonEmail.setStartTLSRequired(config.opt[Boolean]("mail.startTLSRequired").getOrElse(true))
-        commonEmail.setStartTLSEnabled(config.opt[Boolean]("startTLSEnabled").getOrElse(true))
-        commonEmail.setSocketConnectionTimeout(config.opt[Int]("mail.socketConnectionTimeout").getOrElse(4000))
-        commonEmail.setSocketTimeout(config.opt[Int]("mail.socketTimeout").getOrElse(4000))
-        commonEmail
-      }
       commonEmail.
         fold(Try[String](throw new Exception("Email service is not configured.")))(m =>
         Try[String](m.send))
