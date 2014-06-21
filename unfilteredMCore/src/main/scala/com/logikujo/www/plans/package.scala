e4e0063638627ebe6ff8056e93187fcb17da38df
Aitor P. Iturri <aitor.iturri@gmail.com>
2014-06-21 20:25:38 +0200

    Added templates from config to RootPlan and NotFoundPlan

--- unfilteredMCore/src/main/scala/com/logikujo/www/plans/package.scala
+++ unfilteredMCore/src/main/scala/com/logikujo/www/plans/package.scala
@@ -10,37 +10,46 @@ import unfiltered.response._
 import unfiltered.filter.request._
 import scalaz._
 import Scalaz._
+import org.slf4j.LoggerFactory
+
 /**
  *
  * aitoriturri / LogiDev - [Fun Functional] / Logikujo.com
  *
  * com.logikujo.www.plans 17/05/14 :: 15:41 :: eof
  *
- */
-/*
- *
- *
- *
- *
+ * TODO: Add support for disjunction in indexTpml
+ * TODO: Add support for development flag in indelTmpl
  *
  */
 package object plans {
-  def RootPlan[Tag]: Tag #> Plan.Intent =
+  val logger = LoggerFactory.getLogger("unfilteredM.plans")
+  def RootPlan[App](path: String = "/"): App #> Plan.Intent =
     for {
-      scalate <- scalateM[Tag]
+      config <- configM[App].flatMapK(_.atPath(path))
+      scalate <- scalateM[App]
     } yield Intent {
       case req@ContextPath(ctx, "/") =>
         req.respond(Redirect("index"))
       case req@ContextPath(ctx, "/index.html") =>
         req.respond(Redirect("index"))
-      case req@ContextPath(ctx, "/index") =>
-        req.respond(scalate(req,"index.scaml"))
+      case req@ContextPath(ctx, "/index") => (for {
+          indexTmpl <- config.opt[String]("indexTemplate")
+        } yield indexTmpl).
+          map(scalate(req,_)).
+          orElse(Some(InternalServerError ~> ResponseString("indexTemplate"))).
+          foreach(req.respond(_))
     }
 
-  def NotFoundPlan[Tag]: Tag #> Plan.Intent = for {
-    scalate <- scalateM[Tag]
+  def NotFoundPlan[App](path: String = "/"): App #> Plan.Intent = for {
+    config <- configM[App].flatMapK(_.atPath(path))
+    scalate <- scalateM[App]
   } yield Intent {
-      case req@ContextPath(ctx, path) =>
-        req.respond(scalate(req,"404.scaml"))
+      case req@ContextPath(ctx, path) => (for {
+        notFoundTemplate <- config.opt[String]("404Template")
+      } yield notFoundTemplate).
+        map(scalate(req,_)).
+        orElse(Some(InternalServerError ~> ResponseString("404Template"))).
+        foreach(req.respond(_))
     }
 }
